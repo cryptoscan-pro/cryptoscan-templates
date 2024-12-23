085b0c999e93ad3b268ba1a03ece41fa31bbb9d7
Dan Cryptoscan <dancryptoscan@gmail.com>
2024-12-23 20:44:19 +0700

    refactor(clients.json, liquidations.js): replace aiPrompt with templatePath in clients.json and refactor getExchangeUrl function for better readability and maintainability
    This change improves the structure of the clients.json file by replacing the lengthy aiPrompt with a more concise templatePath. Additionally, the getExchangeUrl function is refactored to enhance clarity and organization, making it easier to manage exchange URLs.

--- liquidations.js
+++ liquidations.js
@@ -14,86 +14,87 @@ function getBigNumber(value) {
     return `${value.toFixed(1).replace(/\.0$/, '')}${suffixes[index]}`;
 }
 
-switch (exchange.toLowerCase()) {
-    case "bybit": {
-        return `https://www.bybit.com/ru-RU/trade/spot/${to}/${from}`;
+export function getExchangeUrl(exchange, to, from) {
+    switch (exchange.toLowerCase()) {
+        case "bybit": {
+            return `https://www.bybit.com/ru-RU/trade/spot/${to}/${from}`;
+        }
+        case "bybit-futures": {
+            return `https://www.bybit.com/ru-RU/trade/usdt/${to}${from}`;
+        }
+        case "gateio": {
+            return `https://www.gate.io/ru/trade/${to}_${from}`;
+        }
+        case "gateio-futures": {
+            return `https://www.gate.io/ru/futures/USDT/${to}_${from}`;
+        }
+        case "binance": {
+            return `https://www.binance.com/ru/trade/${to}_${from}`;
+        }
+        case "binance-futures": {
+            return `https://www.binance.com/ru/futures/${to}${from}`;
+        }
+        case "mexc": {
+            return `https://mexc.co/ru-RU/exchange/${to}_${from}`;
+        }
+        case "mexc-futures": {
+            return `https://futures.mexc.com/ru-RU/exchange/${to}_${from}`;
+        }
+        case "htx": {
+            return `https://www.htx.com/ru-ru/trade/${to.toLowerCase()}_${from.toLowerCase()}?type=spot`;
+        }
+        case "kucoin": {
+            return `https://www.kucoin.com/ru/trade/${to}-${from}`;
+        }
+        case "bitmex": {
+            return `https://www.bitmex.com/app/trade/${to}_${from}`;
+        }
+        case "lbank": {
+            return `https://www.lbank.com/trade/${to.toLowerCase()}_${from.toLowerCase()}`;
+        }
+        case "cex": {
+            return `https://cex.io/prices/${from}`;
+        }
+        case "bitmart": {
+            return `https://www.bitmart.com/trade/en-US?symbol=${to}_${from}`;
+        }
+        case "poloniex": {
+            return `https://poloniex.com/trade/${to}_${from}/?type=spot`;
+        }
+        case "ascendex": {
+            return `https://ascendex.com/ru/cashtrade-spottrading/${to}/${from}`;
+        }
+        case "digifinex": {
+            return `https://www.digifinex.com/ru-ru/trade/${to}/${from}`;
+        }
+        case "okx": {
+            return `https://www.okx.com/ru/trade-spot/${to}-${from}`;
+        }
+        case "bitget": {
+            return `https://www.bitget.com/ru/spot/${to}${from}`;
+        }
+        case "bitget-futures": {
+            return `https://www.bitget.com/ru/futures/usdt/${to}${from}`;
+        }
+        case "coinex": {
+            return `https://coinex.com/en/exchange/${to}-${from}`;
+        }
+        case "coinex-futures": {
+            return `https://www.coinex.com/en/futures/${to}-${from}`;
+        }
+        case "bitmex": {
+            return `https://www.bitmex.com/app/trade/${to}${from}`;
+        }
+        case "bitmex-futures": {
+            return `https://www.bitmex.com/app/trade/${to}${from}`;
+        }
+        default: {
+            return `https://cryptoscan.pro/swap`;
+        }
     }
-    case "bybit-futures": {
-        return `https://www.bybit.com/ru-RU/trade/usdt/${to}${from}`;
-    }
-    case "gateio": {
-        return `https://www.gate.io/ru/trade/${to}_${from}`;
-    }
-    case "gateio-futures": {
-        return `https://www.gate.io/ru/futures/USDT/${to}_${from}`;
-    }
-    case "binance": {
-        return `https://www.binance.com/ru/trade/${to}_${from}`;
-    }
-    case "binance-futures": {
-        return `https://www.binance.com/ru/futures/${to}${from}`;
-    }
-    case "mexc": {
-        return `https://mexc.co/ru-RU/exchange/${to}_${from}`;
-    }
-    case "mexc-futures": {
-        return `https://futures.mexc.com/ru-RU/exchange/${to}_${from}`;
-    }
-    case "htx": {
-        return `https://www.htx.com/ru-ru/trade/${to.toLowerCase()}_${from.toLowerCase()}?type=spot`;
-    }
-    case "kucoin": {
-        return `https://www.kucoin.com/ru/trade/${to}-${from}`;
-    }
-    case "bitmex": {
-        return `https://www.bitmex.com/app/trade/${to}_${from}`;
-    }
-    case "lbank": {
-        return `https://www.lbank.com/trade/${to.toLowerCase()}_${from.toLowerCase()}`;
-    }
-    case "cex": {
-        return `https://cex.io/prices/${from}`;
-    }
-    case "bitmart": {
-        return `https://www.bitmart.com/trade/en-US?symbol=${to}_${from}`;
-    }
-    case "poloniex": {
-        return `https://poloniex.com/trade/${to}_${from}/?type=spot`;
-    }
-    case "ascendex": {
-        return `https://ascendex.com/ru/cashtrade-spottrading/${to}/${from}`;
-    }
-    case "digifinex": {
-        return `https://www.digifinex.com/ru-ru/trade/${to}/${from}`;
-    }
-    case "okx": {
-        return `https://www.okx.com/ru/trade-spot/${to}-${from}`;
-    }
-    case "bitget": {
-        return `https://www.bitget.com/ru/spot/${to}${from}`;
-    }
-    case "bitget-futures": {
-        return `https://www.bitget.com/ru/futures/usdt/${to}${from}`;
-    }
-    case "coinex": {
-        return `https://coinex.com/en/exchange/${to}-${from}`;
-    }
-    case "coinex-futures": {
-        return `https://www.coinex.com/en/futures/${to}-${from}`;
-    }
-    case "bitmex": {
-        return `https://www.bitmex.com/app/trade/${to}${from}`;
-    }
-    case "bitmex-futures": {
-        return `https://www.bitmex.com/app/trade/${to}${from}`;
-    }
-    default: {
-        return `https://cryptoscan.pro/swap`;
-    }
-}
 }
 
-export default function(data) {
+export default function (data) {
     const type = data.variant === 'long' ? 'ЛОНГ 📉' : 'ШОРТ 📈';
     const symbol = data.symbol.replace('$', '').replace('#', '').toUpperCase();
     return `💥 Ликвидирован #${symbol} ${type} на ${getBigNumber(data.usd)} USDT на [${data.exchange}](${getExchangeUrl(data.exchange, symbol, 'USDT')}) при цене $${data.price}`
