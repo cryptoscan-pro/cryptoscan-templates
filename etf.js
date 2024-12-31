00498c6cfd8d34e63a85ecbaa28ada8a3bbae4b4
Dan Cryptoscan <dancryptoscan@gmail.com>
2024-12-31 08:48:28 +0700

    fix(etf.js): use Math.abs for flows in return statement to ensure positive values for inflow and outflow messages

--- etf.js
+++ etf.js
@@ -18,5 +18,5 @@ export default function(data) {
     const label = data.flows > 0 ? '🌡' : '🧪';
     const symbol = data.ticker || data.symbol;
 
-    return `${label} ETF ${data.flows > 0 ? 'INFLOW' : 'OUTFLOW'} ${getBigNumber(data.flows)} USDT from #${symbol} by #${data.issuer} vol: ${getBigNumber(data.volume)}\n#${data.asset} #${data.custodian} #${data.chain}`
+    return `${label} ETF ${data.flows > 0 ? 'INFLOW' : 'OUTFLOW'} ${getBigNumber(Math.abs(data.flows))} USDT from #${symbol} by #${data.issuer} vol: ${getBigNumber(data.volume)}\n#${data.asset} #${data.custodian} #${data.chain}`
 }
