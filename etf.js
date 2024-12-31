8b614ab7cb77ae456b65e52fbdb64800a1486fc3
Dan Cryptoscan <dancryptoscan@gmail.com>
2024-12-31 08:22:37 +0700

    feat(clients.json): add new ETF client configuration for announcements feat(etf.js): create ETF template to format and display ETF inflow/outflow data

---
+++ etf.js
@@ -0,0 +1,22 @@
+function getBigNumber(value) {
+    if (typeof value !== 'number') {
+        return '0';
+    }
+
+    const suffixes = ['', 'K', 'M', 'B'];
+    let index = 0;
+
+    while (value >= 1000 && index < suffixes.length - 1) {
+        value /= 1000;
+        index++;
+    }
+
+    return `${value.toFixed(1)?.replace(/\.0$/, '')}${suffixes[index]}`;
+}
+
+export default function(data) {
+    const label = data.flows > 0 ? '🌡' : '🧪';
+    const symbol = data.ticker || data.symbol;
+
+    return `${label} ETF ${data.flows > 0 ? 'INFLOW' : 'OUTFLOW'} ${getBigNumber(data.flows)} USDT from #${symbol} by #${data.issuer} vol: ${getBigNumber(data.volume)}\n#${data.asset} #${data.custodian} #${data.chain}`
+}
