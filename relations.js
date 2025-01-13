fb1e59645a2d27c95eda692dfc2f740fa51341d8
Dan Cryptoscan <dancryptoscan@gmail.com>
2025-01-13 19:48:48 +0700

    feat(relations.js): add utility function to format 'createdAt' values for better readability in output refactor(relations.js): update text generation to use getValueByKey for consistent value formatting

--- relations.js
+++ relations.js
@@ -1,5 +1,14 @@
+import { getAgo } from "./utils/getAgo";
+
 async function fetchItem(id) {
     return fetch(`https://api.cryptoscan.pro?id=${id}`).then((r) => r.json()).then((r) => r.data[0])
+}
+
+function getValueByKey(key, value) {
+    if (key === 'createdAt') {
+        return getAgo(value);
+    }
+    return value;
 }
 
 export default async function(data) {
@@ -8,9 +17,9 @@ export default async function(data) {
     let text = '';
 
     text += 'BEFORE\n'
-    text += Object.entries(itemFrom).map(([key, value]) => `${key}=${value}`).join('\n')
+    text += Object.entries(itemFrom).map(([key, value]) => `${key}=${getValueByKey(value)}`).join('\n')
     text += '\n\nAFTER\n'
-    text += Object.entries(itemTo).map(([key, value]) => `${key}=${value}`).join('\n')
+    text += Object.entries(itemTo).map(([key, value]) => `${key}=${getValueByKey(value)}`).join('\n')
     text += `\n\nRELATION: ${data.field}`;
     return text;
 }
