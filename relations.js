6cb5f0c7987365cc2834f3dfb738cdc1516ddc08
Dan Cryptoscan <dancryptoscan@gmail.com>
2025-01-13 17:28:12 +0700

    feat(relations.js): refactor export function to fetch data from API and format output for better insights chore(clients.json): remove timeout property to simplify client configuration and avoid unnecessary complexity

--- relations.js
+++ relations.js
@@ -1,3 +1,16 @@
-export default function(data) {
-    return `FROM: ${data.from}\nTO: ${data.to}\nRELATION: ${data.field}`
+async function fetchItem(id) {
+    return fetch(`https://api.cryptoscan.pro?id=${id}`).then((r) => r.json()).then((r) => r.data[0])
+}
+
+export default async function(data) {
+    const itemFrom = await fetchItem(data.from);
+    const itemTo = await fetchItem(data.to);
+    let text = '';
+
+    text += 'BEFORE\n'
+    text += Object.entries(itemFrom).map(([key, value]) => `${key}=${value}`).join('\n')
+    text += '\n\nAFTER\n'
+    text += Object.entries(itemTo).map(([key, value]) => `${key}=${value}`).join('\n')
+    text += `\n\nRELATION: ${data.field}`;
+    return text;
 }
